bbbfb46cfe726076c318c4de3b0e582193676bac
xiaophai <1379347218@qq.com>
2024-01-13 18:18:59 +0800

    furlongs -> yards

--- chapter02/2-2.cpp
+++ chapter02/2-2.cpp
@@ -3,8 +3,11 @@ using namespace std;
 
 int main()
 {
-  int a;
-  cin >> a;
-  cout << a;
+  int furlongs;
+  cout << "Input a distance in furlongs: ";
+  cin >> furlongs;
+  int yards = furlongs * 220;
+  cout << furlongs << " (furlongs) = " 
+  << yards << " (yards)" << endl;
   return 0;
 }
